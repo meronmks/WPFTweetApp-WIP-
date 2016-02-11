d9a0a84ac884a499a6d935f420ad2414391009d9
meronmks <meronmks.8914@gmail.com>
2016-02-12 01:38:04 +0900

    firstcommit

---
+++ TweetApp/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CoreTweet;
+using TweetApp.CoreClass;
+using UniRx;
+
+namespace TweetApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public OAuth.OAuthSession Session { get; set; }
+        private Tokens tokens { get; set; }
+        public List<Status> status { get; set; }
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            status = new List<Status>();
+            if (Properties.Settings.Default.AccessToken != "" &&
+                Properties.Settings.Default.AccessTokenSecret != "")
+            {
+                tokens = Tokens.Create(
+                    TwitterProperties.APIKey,
+                    TwitterProperties.APISecret,
+                    Properties.Settings.Default.AccessToken,
+                    Properties.Settings.Default.AccessTokenSecret);
+            }
+            else
+            {
+                var OauthWindow = new OauthWindow();
+                OauthWindow.Show();
+                Close();
+            }
+        }
+
+        private async void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var tweet in await tokens.Statuses.HomeTimelineAsync())
+            {
+                status.Add(tweet);
+            }
+
+            listBox.ItemsSource = status;
+        }
+    }
+}
---
+++ TweetApp/OauthWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CoreTweet;
+using TweetApp.CoreClass;
+
+namespace TweetApp
+{
+    /// <summary>
+    /// OauthWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class OauthWindow : Window
+    {
+        private OAuth.OAuthSession Session { get; set; }
+        private Tokens tokens = TwitterProperties.tokens;
+
+        public OauthWindow()
+        {
+            InitializeComponent();
+
+            Session = OAuth.Authorize(TwitterProperties.APIKey, TwitterProperties.APISecret);
+            WebBrowser.Source = Session.AuthorizeUri;
+        }
+
+        private void AuthorizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tokens = Session.GetTokens(PinBox.Text);
+                Properties.Settings.Default.AccessToken = tokens.AccessToken;
+                Properties.Settings.Default.AccessTokenSecret = tokens.AccessTokenSecret;
+                Properties.Settings.Default.Save();
+                var result = MessageBox.Show("認証成功！", "Success", MessageBoxButton.OK);
+                if (result == MessageBoxResult.OK)
+                {
+                    var MainWindow = new MainWindow();
+                    MainWindow.Show();
+                    Close();
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("認証失敗。PINを再入力してください。", "Error", MessageBoxButton.OK);
+            }
+        }
+    }
+}
